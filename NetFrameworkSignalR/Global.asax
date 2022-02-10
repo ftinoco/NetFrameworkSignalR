b896868313cbd1560e13c472b5298bb3a502fee2
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2022-02-09 21:59:43 -0600

    Add project files.

---
+++ NetFrameworkSignalR/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NetFrameworkSignalR.MvcApplication" Language="C#" %>
---
+++ NetFrameworkSignalR/MessagesHub .cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace NetFrameworkSignalR
+{
+    [HubName("messagesHub")]   
+    public class MessagesHub : Hub
+    {
+        public static void SendMessages()
+        {
+            IHubContext context = GlobalHost.ConnectionManager.GetHubContext<MessagesHub>();
+        }
+
+        [HubMethodName("send")]
+        public void Send(string name)
+        {
+            string salt = "MyName";
+            IHubContext context = GlobalHost.ConnectionManager.GetHubContext<MessagesHub>();
+            context.Clients.All.updateMessages(salt.Equals(name));
+        }
+    }
+}
